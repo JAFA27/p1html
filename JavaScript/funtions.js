909dae04e87324c82489fc914a760e8cdd326fca
JAFA27 <felixacunajosealonso@gmail.com>
2024-02-09 12:07:53 -0600

    Se agrego el msj de hola amigo y un pequeño cuestionario

--- JavaScript/funtions.js
+++ JavaScript/funtions.js
@@ -1,4 +1,8 @@
 function changeColor(){
   document.getElementById("Documento").style.backgroundColor="green";
+  document.getElementById("Documento").textContent="hola"
+}
 
+function enviarMsg(){
+  alert("hola amigo"); 
 }
